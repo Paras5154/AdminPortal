e0fe5d1d5989d06d6a641f71615271915d180d76
ChocoMeltz <Paggarwal5154@gmail.com>
2023-10-05 10:18:49 +0530

    initial commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>ChocoMeltz</span>
+      <span>© 2023 ChocoMeltz Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
